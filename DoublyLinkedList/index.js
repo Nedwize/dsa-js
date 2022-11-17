a933aac32f95431885619830e30a468cfdc9bcbf
Nedwize <nakshatra163@gmail.com>
2022-11-17 21:59:11 +0530

    feat: add pop method

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -7,6 +7,20 @@ class DoublyLinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    push(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.prev = this.tail
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
 }
 
 module.exports = DoublyLinkedList
