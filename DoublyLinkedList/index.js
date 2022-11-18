9888f2b97c062e91ae3a71bd1d2847997542e955
Nedwize <nakshatra163@gmail.com>
2022-11-18 19:12:19 +0530

    feat: add unshift method

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -40,6 +40,20 @@ class DoublyLinkedList {
         this.length--
         return temp
     }
+
+    unshift(value) {
+        const newNode = new Node(value)
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
 }
 
 module.exports = DoublyLinkedList
