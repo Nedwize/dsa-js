2be970f55f2f52f228096b54a03c7c9187bffa60
Nedwize <nakshatra163@gmail.com>
2022-11-28 20:45:13 +0530

    feat: add shift method

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -54,6 +54,21 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+
+    shift() {
+        if (this.length === 0) return undefined
+        const temp = this.head
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            temp.next = null
+        }
+        this.length--
+        return temp
+    }
 }
 
 module.exports = DoublyLinkedList
